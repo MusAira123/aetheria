bf90220fcbe6d7a9d777e63b19dae704f8791344
MusAira123 <108538116+MusAira123@users.noreply.github.com>
2026-05-09 01:47:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
             <img
               src="/logo.png"
               className="h-64 md:h-56 lg:h-80 object-contain"
-            />
+            />Mun
 
           </div>
 
